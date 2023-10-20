c340432b7992aa46332026f241164bc909a1708f
junio teotonio dos santos <juniots04@gmail.com>
2023-10-20 17:11:51 -0300

    rotas corrigidas

--- Megaproject/urls.py
+++ Megaproject/urls.py
@@ -1,9 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path
 from core.views import mega
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mega, name= 'mega')
+    path('', mega, name= 'mega'),
+    path('404/', TemplateView.as_view(template_name='404.html', name='404'))
+
 ]
